7bef1c09d989eeb5707222f9dc13790b9c17540e
Gerardo Garduno <48225150+GerardoGarduno@users.noreply.github.com>
2024-03-12 09:29:45 -0700

    Spit data into two teams
    Shows who won and who lost.. Bug:Because im getting the PUUID by summnors name, if the player has a duplicate name like e.g angel it wont show becuase riot has allowed for duplciate account names. I think this is why they recmonned not using get PUUID by summoners but ok.

--- Riot Api/LOLSTATS/src/components/Body.jsx
+++ Riot Api/LOLSTATS/src/components/Body.jsx
@@ -10,23 +10,39 @@ function Body({ gameList }) {
             {gameList.map((gameData, index) =>
               <div key={index} className="mb-4">
                 <h2 className="text-lg font-semibold mb-2">Game {index + 1}</h2>
-                <div className="">
-                  {gameData.info.participants.map((data, participantIndex) =>
-                    <div key={participantIndex} className={`flex items-center rounded-lg p-2 mb-2 ${participantIndex < 5 ?  'bg-blue-200' : 'bg-red-200'}`}>
-                      {console.log(data.championName)}
+                <div className= " flex gap-2 text-lg font-semibold mb-2">
+                    Team 1: {gameData.info.teams[0].win ? <h3 className='text-green-600 '>Won</h3>: <p className='text-red-600'>Lost</p>}
+                </div>
+                  {gameData.info.participants.slice(0, 5).map((data, participantIndex)=>
+                    <div key={participantIndex} className= "flex items-cente bg-blue-200 rounded-lg p-2 mb-2">
                       <img
                         src={`https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/${data.championName}.png`}
                         alt={`Champion ${data.championName}`}
-                        className="w-8 h-8 mr-2"
+                        className="w-10 h-10 mr-2"
                       />
-                      <div>
+                       <div>
+                        <span className="font-semibold">Player {participantIndex + 1}: </span>
+                        {console.log(data.summonerName)}
+                        <span className="mr-2">{data.summonerName}, Champion: {data.championName}, KDA: <span className='text-green-900'>{data.kills}</span> / <span className="text-red-600">{data.deaths}</span> / {data.assists}</span>
+                      </div>
+                    </div>
+                  )}
+                  <div className="flex gap-2 text-lg font-semibold mb-2">
+                      Team 2: {gameData.info.teams[1].win ? <h3 className='text-green-600 '>Won</h3>: <p className='text-red-600'>Lost</p>}
+                  </div>                  
+                  {gameData.info.participants.slice(5, 10).map((data, participantIndex)=>
+                    <div key={participantIndex} className= "flex items-cente bg-red-200 rounded-lg p-2 mb-2">
+                      <img
+                        src={`https://ddragon.leagueoflegends.com/cdn/14.5.1/img/champion/${data.championName}.png`}
+                        alt={`Champion ${data.championName}`}
+                        className="w-10 h-10 mr-2"
+                      />
+                       <div>
                         <span className="font-semibold">Player {participantIndex + 1}: </span>
                         <span className="mr-2">{data.summonerName}, Champion: {data.championName}, KDA: <span className='text-green-900'>{data.kills}</span> / <span className="text-red-600">{data.deaths}</span> / {data.assists}</span>
                       </div>
                     </div>
-                    
                   )}
-                </div>
               </div>
             )}
           </div> :
